43add0a18308dc1c38ce30f9837871fbe7f0f8ba
Cindy <karanjacindy1@gmail.com>
2023-03-16 10:12:57 -0700

    Updated

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Code entry
+ * Description: C program to print the size of data types.
+ * Return: 0
+ */
+
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long int);
+	sizeof(long long int);
+	sizeof(float);
+	return (0);
+}
+
+
