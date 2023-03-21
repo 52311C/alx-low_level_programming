0878369086bbb17a732d19cec3d7bd1a25fba169
Cindy <karanjacindy1@gmail.com>
2023-03-21 07:25:13 -0700

    Next task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  Code entry
+ * Description: Print alphabet X10
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+}
+
