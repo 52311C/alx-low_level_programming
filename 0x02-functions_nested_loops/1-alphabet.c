b315ccd187c2ab5e7efdcddee09bc1706b655594
Cindy <karanjacindy1@gmail.com>
2023-03-21 06:50:33 -0700

    Updated

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Code entry
+ * function print_alphabet: Print alphabet
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+
+	return (0);
+
+}
+
