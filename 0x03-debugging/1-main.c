695492c1131196a8d13981eda7ead7458c0fd99c
Cindy <karanjacindy1@gmail.com>
2023-03-22 02:29:40 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,16 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
+	
 	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
-	while (i < 10)
-	{
-	putchar(i);
-/*i has not been incremented. Creates infinite loop.*/
-	}
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
