261062ea6466f31190ee6c8a36c1d3efadb6b3c4
Cindy <karanjacindy1@gmail.com>
2023-03-23 06:14:45 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - code entry
- * Description: Print 0 - 9
+ * print_numbers - print  0 - 9
+ *
  * Return: void
  */
 
 void print_numbers(void)
 {
-	char q;
+	char a = 0;
 
-	while (q <= 9)
+	while (a <= 9)
 	{
-		_putchar(q);
-		q++;
+		_putchar(a + '0');
+		a++;
 	}
 	_putchar('\n');
 }
