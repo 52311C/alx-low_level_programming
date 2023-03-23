74fe674919d46f3c4a769a684d67f674d44eb86f
Cindy <karanjacindy1@gmail.com>
2023-03-23 06:46:51 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers -  Print 0-14 X10
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	while (b <= 10)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar(a + '0');
+		}
+		_putchar('\n');
+		b++;
+	}
+}
