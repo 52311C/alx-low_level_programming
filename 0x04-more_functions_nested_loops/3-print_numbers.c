16838cd4ed666b2f88fa5cb619e2c64c644c6f95
Cindy <karanjacindy1@gmail.com>
2023-03-23 06:03:34 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - code entry
+ * Description: Print 0 - 9
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+	int q;
+
+	for (q = 0; q < 10; q++)
+	{
+		_putchar(q);
+	}
+	_putchar('\n');
+}
