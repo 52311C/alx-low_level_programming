660a752ab09be170d8ec12550c07b12f79536717
Cindy <karanjacindy1@gmail.com>
2023-04-05 05:21:16 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  a function that prints a string
+ * @s: String value
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+}
