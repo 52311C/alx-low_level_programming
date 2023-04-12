7eb0cba678c77f4a7e8f5f6660daef4b37a610a2
Cindy <karanjacindy1@gmail.com>
2023-04-12 02:12:44 -0700

    Split string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * strtow -  splits the string into two
+ * @str: string value to be split
+ * Return: NUll if str == NULL || str ==""
+ */
+
+char **strtow(char *str)
+{
+}
