8face0ace466fa81aa67c6c1fad13f0e31b6eaf2
Cindy <karanjacindy1@gmail.com>
2023-04-19 11:06:00 -0700

    Updates

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/**
- * file - stdlib.h
- * Description: Header file
- */
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
