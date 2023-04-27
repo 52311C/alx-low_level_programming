7eac68f36c8a31d80eede61bd930349120970655
Cindy <karanjacindy1@gmail.com>
2023-04-27 14:16:07 -0700

    Story

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * story - Prints a string
+ * Return: void
+ */
+void story(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my houe upon my back!\n");
+}
