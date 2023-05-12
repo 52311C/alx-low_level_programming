b36225f40640f0c71089c2a18d7947b523499077
52311C <karanjacindy1@gmail.com>
2023-05-12 13:34:21 -0700

    Updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
-	if (fd < 0)
+	if (fd == -1)
 	{
 		return (-1);
 	}
@@ -28,11 +28,12 @@ int create_file(const char *filename, char *text_content)
 		{
 		}
 	}
-	nwrite = write(0, text_content, len);
+	nwrite = write(fd, text_content, len);
 	if (nwrite == -1)
 	{
 		close(fd);
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
